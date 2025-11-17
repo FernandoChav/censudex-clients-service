74706d1fd0163295fea4d5f8fbbcefac3f076dfc
Fernando Chávez <fpatric25@gmail.com>
2025-11-16 21:53:41 -0300

    fix(mapping): fix UpdateCLietRequest to client

--- ClientsService/Profiles/MappingProfile.cs
+++ ClientsService/Profiles/MappingProfile.cs
@@ -15,23 +15,23 @@ public class MappingProfile : Profile
             .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.PhoneNumber));
 
         CreateMap<UpdateClientRequest, Client>()
-             .ForMember(dest => dest.Id, opt => opt.Ignore())
-             .ForMember(dest => dest.PasswordHash, opt => opt.Ignore())
+            .ForMember(dest => dest.Id, opt => opt.Ignore())
+            .ForMember(dest => dest.PasswordHash, opt => opt.Ignore())
 
-             // 1. Arreglo del Crash de Fecha: Solo parsear si NO es vacío
-             .ForMember(dest => dest.BirthDate, opt =>
-             {
-                 opt.Condition(src => !string.IsNullOrEmpty(src.BirthDate));
-                 opt.MapFrom(src => DateOnly.Parse(src.BirthDate));
-             })
+            // 1. Arreglo del Crash de Fecha: Solo parsear si NO es vacío
+            .ForMember(dest => dest.BirthDate, opt =>
+            {
+                opt.Condition(src => !string.IsNullOrEmpty(src.BirthDate));
+                opt.MapFrom(src => DateOnly.Parse(src.BirthDate));
+            })
 
-             // 2. Arreglo para no borrar datos: Solo mapear si tienen texto
-             .ForMember(dest => dest.FirstName, opt => opt.Condition(src => !string.IsNullOrEmpty(src.FirstName)))
-             .ForMember(dest => dest.LastName, opt => opt.Condition(src => !string.IsNullOrEmpty(src.LastName)))
-             .ForMember(dest => dest.Email, opt => opt.Condition(src => !string.IsNullOrEmpty(src.Email)))
-             .ForMember(dest => dest.Username, opt => opt.Condition(src => !string.IsNullOrEmpty(src.Username)))
-             .ForMember(dest => dest.Address, opt => opt.Condition(src => !string.IsNullOrEmpty(src.Address)))
-             .ForMember(dest => dest.PhoneNumber, opt => opt.Condition(src => !string.IsNullOrEmpty(src.PhoneNumber)));
+            // 2. Arreglo para no borrar datos: Solo mapear si tienen texto
+            .ForMember(dest => dest.FirstName, opt => opt.Condition(src => !string.IsNullOrEmpty(src.FirstName)))
+            .ForMember(dest => dest.LastName, opt => opt.Condition(src => !string.IsNullOrEmpty(src.LastName)))
+            .ForMember(dest => dest.Email, opt => opt.Condition(src => !string.IsNullOrEmpty(src.Email)))
+            .ForMember(dest => dest.Username, opt => opt.Condition(src => !string.IsNullOrEmpty(src.Username)))
+            .ForMember(dest => dest.Address, opt => opt.Condition(src => !string.IsNullOrEmpty(src.Address)))
+            .ForMember(dest => dest.PhoneNumber, opt => opt.Condition(src => !string.IsNullOrEmpty(src.PhoneNumber)));
 
         CreateMap<Client, ClientResponse>()
             .ForMember(dest => dest.FullName, opt => opt.MapFrom(src => $"{src.FirstName} {src.LastName}"))
